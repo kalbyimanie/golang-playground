8737f61492b3fc58b715700223fd8f7761999fd2
kalbyimanie <aby.imanie@gmail>
2023-08-01 07:04:29 +0700

    sample

--- main.go
+++ main.go
@@ -4,27 +4,43 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
 
-func readContentFromLastCommit(filename string) ([]byte, error) {
-	cmd := exec.Command("git", "show", "HEAD:"+filename)
+func readNewLinesFromLastCommit(filename string) ([]string, error) {
+	cmd := exec.Command("git", "diff", "HEAD^", "--", filename)
 	output, err := cmd.Output()
 	if err != nil {
 		return nil, err
 	}
 
-	return output, nil
+	var newLines []string
+	lines := strings.Split(string(output), "\n")
+
+	for _, line := range lines {
+		if strings.HasPrefix(line, "+") && !strings.HasPrefix(line, "+++") {
+			newLines = append(newLines, strings.TrimPrefix(line, "+"))
+		}
+	}
+
+	return newLines, nil
 }
 
 func main() {
 	filename := "yourfile.txt"
 
-	content, err := readContentFromLastCommit(filename)
+	lines, err := readNewLinesFromLastCommit(filename)
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 
-	fmt.Println("Content of the file from the last commit:")
-	fmt.Println(string(content))
+	if len(lines) == 0 {
+		fmt.Println("No new lines were added to the file in the last commit.")
+	} else {
+		fmt.Println("New lines added to the file in the last commit:")
+		for _, line := range lines {
+			fmt.Println(line)
+		}
+	}
 }
